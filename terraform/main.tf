ffdaa018235671a50acec8ce5c0a663d3d41b586
Ronnie Smith <ronnie@deltalinks.com>
2025-07-18 14:55:32 -0500

    Fix Terraform formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,10 +13,10 @@ resource "aws_s3_bucket" "frontend" {
 }
 
 resource "aws_s3_bucket_public_access_block" "frontend" {
-  bucket = aws_s3_bucket.frontend.id
-  block_public_acls = false
-  block_public_policy = false
-  ignore_public_acls = false
+  bucket                  = aws_s3_bucket.frontend.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
   restrict_public_buckets = false
 }
 
@@ -26,10 +26,10 @@ resource "aws_s3_bucket_policy" "frontend_policy" {
     Version = "2012-10-17",
     Statement = [
       {
-        Effect = "Allow",
+        Effect    = "Allow",
         Principal = "*",
-        Action = "s3:GetObject",
-        Resource = "${aws_s3_bucket.frontend.arn}/*"
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.frontend.arn}/*"
       }
     ]
   })
