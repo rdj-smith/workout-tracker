1c0d15d9a13baf0533784723c815cfa873f3feee
Ronnie Smith <ronnie@deltalinks.com>
2025-07-18 14:46:41 -0500

    Add CloudFront + ACM integration to Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -119,3 +119,72 @@ output "s3_website_url" {
 output "api_url" {
   value = aws_apigatewayv2_api.http_api.api_endpoint
 }
+
+variable "acm_certificate_arn" {
+  type    = string
+  default = "arn:aws:acm:us-east-1:774874928453:certificate/07dd439d-3eeb-4b76-9cad-86f99c2a3c51"
+}
+
+variable "custom_domain" {
+  type    = string
+  default = "workout.deltalinks.com"
+}
+
+resource "aws_cloudfront_origin_access_identity" "s3_oai" {}
+
+resource "aws_cloudfront_distribution" "frontend" {
+  origin {
+    domain_name = aws_s3_bucket.frontend.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.frontend.id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.s3_oai.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Workout tracker frontend"
+  default_root_object = "index.html"
+
+  aliases = [var.custom_domain]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.frontend.id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = var.acm_certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Name = "WorkoutTrackerFrontendCDN"
+  }
+}
+
+output "cloudfront_url" {
+  value = aws_cloudfront_distribution.frontend.domain_name
+}
